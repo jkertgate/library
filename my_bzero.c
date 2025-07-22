d2b2e93212ecd34d99fbc62bcee896c6586c4729
jkertgate <jeremykertgate185@gmail.com>
2025-07-22 16:20:34 -0400

    bzero

--- my_bzero.c
+++ my_bzero.c
@@ -1,12 +1,9 @@
 #include "libft.h"
 
-void	my_bzero(void *s, size_t n)
+void ft_bzero(void *s, size_t n)
 {
-	size_t			i;
-	unsigned char	*tmp;
+	unsigned char *ptr = (unsigned char *)s;
 
-	i = 0;
-	tmp = (unsigned char*)s;
-	while (i < n)
-		tmp[i++] = '\0';
+	while (n--)
+		*ptr++ = 0;
 }
